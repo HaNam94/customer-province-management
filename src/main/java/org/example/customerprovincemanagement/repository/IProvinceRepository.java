e5254d9229687200149f07809cf05f77aa372cc1
HaNam94 <haduynam.94@gmail.com>
2024-06-19 16:45:49 +0700

    JPA repository

---
+++ src/main/java/org/example/customerprovincemanagement/repository/IProvinceRepository.java
@@ -0,0 +1,8 @@
+package org.example.customerprovincemanagement.repository;
+
+
+import org.example.customerprovincemanagement.model.Province;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IProvinceRepository extends CrudRepository<Province, Long> {
+}
---
+++ src/main/java/org/example/customerprovincemanagement/service/IProvinceService.java
@@ -0,0 +1,7 @@
+package org.example.customerprovincemanagement.service;
+
+
+import org.example.customerprovincemanagement.model.Province;
+
+public interface IProvinceService extends IGenerateService<Province> {
+}
